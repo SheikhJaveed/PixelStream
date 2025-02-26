8212107ddf28aac26f8d055fdc65216205057aa4
Sheikh Javeed <sheikhmannanjaveed@gmail.com>
2025-02-26 21:22:40 +0530

    added files in source folder

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const app=express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials:true
+    })
+)
+
+//common middleware
+app.use(express.json({"limit":"30mb"}));
+app.use(express.urlencoded({"extended":true,limit:"30mb"}));
+app.use(express.static("public"));
+export {app};
